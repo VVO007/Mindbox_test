b8e47e95b3ac71171d31b44dfe3de9f62cb37be2
VVO007 <112197152+VVO007@users.noreply.github.com>
2022-11-14 15:39:08 +0300

    Add files via upload
    Тестовые задания на позицию Стажера

---
+++ Площадь треугольника.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NewLibrary
+{
+    public class Triangle
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public Triangle(double a,double b,double c)
+        {
+            SideA = a;
+            SideB = b;
+            SideC = c;
+            if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2))
+            {
+                Console.WriteLine("Треугольник прямоугольный");
+            }
+        }
+        public override double GetArea()
+        {
+            double p = GetPerimeter()/2;
+            Area = Math.Sqrt(p*(p-SideA)*(p - SideB) *(p - SideC));
+            return Area;
+        }
+
+    }
+}
---
+++ ПлощадьКруга.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NewLibrary
+{
+    public class Circle
+    {
+        public double radius { get; set; }
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        public override double GetArea()
+        {
+            Area = Math.PI*Math.Pow(radius,2);
+            return Area;
+        }
+    }
+}
